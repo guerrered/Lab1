512f47291735499a6252e14850916e0c9a095cce
dishenwen <dwen@uwm.edu>
2017-02-01 14:29:33 -0600

    2:29

--- src/Scoresheet.java
+++ src/Scoresheet.java
@@ -1,9 +1,28 @@
 
 public class Scoresheet {
 
+int scores=0;
+int Score[];
+//ind 1,2 for throw 1,2. ind3 for score in that frame
+boolean Strike=false;
+int ThrowNum=0;
 	
+public Scoresheet()
+{
+	Score= new int[30];
 	
+}
 	
+public int Throw(int cnt)
+{
+	Score[ThrowNum]=cnt;
+	ThrowNum++;
+	if(ThrowNum%2)
+	return cnt;
+}
+	
+
+
 	
 	
 	
