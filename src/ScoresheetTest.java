7b4ee4a2373467584dfa535ea214a913bcbbfb35
John E Krause <jekrause@uwm.edu>
2017-02-01 14:11:01 -0600

    Scoresheet

--- src/ScoresheetTest.java
+++ src/ScoresheetTest.java
@@ -6,7 +6,67 @@ public class ScoresheetTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+
+
+//		testStrikeOnLastFrames - test throwing a strike on the 8th, 9th, and 10th frames
+//		testThrowOn11thFrame - ensure throwing on the 11th frame is not allowed (in
+//		some form)
+		
+//		testOneThrow - test a single throw and check scoring for the first frame and game
+		@Test
+		public void testOneThrow(){
+			
+		}
+		
+//		testTwoThrows - test two throws and check scoring for the first frame and game
+		@Test
+		public void testTwoThrows(){
+			
+		}
+		
+//		testThreeThrows - test three throws and check scoring for the first two frames and
+//		game
+		@Test
+		public void testThreeThrows(){
+			
+		}
+		
+//		testSpareCountsNextFrameScore - throw a spare in a frame and make sure its
+//		score is correct (counting the following frame, which should also be completed)
+		@Test
+		public void testSpareCountsNextFrameScore(){
+			
+		}
+		
+//		testStrikeMovesToNextFrame - ensure that a strike frame may not have two
+//		throws
+		@Test
+		public void testStrikeMovesToNextFrame(){
+			
+		}
+		
+//		testStrikeCountsNextFrameScores - throw a strike in a frame and make sure its
+//		score is correct (counting the following frames, which should also be completed)
+		@Test
+		public void testStrikeCountsNextFrameScores(){
+			
+		}
+		
+//		testSpareOnLastFrame - test throwing a spare on the 10th frame
+		@Test
+		public void testSpareOnLastFrame(){
+			
+		}
+		
+		@Test
+		public void testOneThrow(){
+			
+		}
+		
+		@Test
+		public void testOneThrow(){
+			
+		}
 	}
 
 }
